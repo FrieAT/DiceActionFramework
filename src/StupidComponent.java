adc88ce10db4e7bc5a778bfdd600937b9954647f
Simon Teufl <s.teufl96@gmx.at>
2021-11-06 09:29:41 +0100

    Graphik positioning works, label works, still flickering.

--- src/StupidComponent.java
+++ src/StupidComponent.java
@@ -4,10 +4,9 @@ public class StupidComponent extends AbstractComponent
     public void update() {
         PictureGraphic g = (PictureGraphic)this.getGameObject().getComponent(EComponentType.AGraphic);
         if(g != null) {
-            g.top = g.top;
-            g.left = g.left;
-            // g.top = (g.top + 1) % 50;
-            // g.left = (g.left - 1) % 50;
+
+             g.top = (g.top + 1) % 50;
+             g.left = (g.left - 1) % 50;
         }
     }
 }
