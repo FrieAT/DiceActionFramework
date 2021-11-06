6ea7cbc13e66ebe82f3e0aff0ce6911b1e7e2a45
steufl <62016015+steufl@users.noreply.github.com>
2021-11-06 09:35:33 +0100

    Update StupidComponent.java

--- src/StupidComponent.java
+++ src/StupidComponent.java
@@ -4,7 +4,6 @@ public class StupidComponent extends AbstractComponent
     public void update() {
         PictureGraphic g = (PictureGraphic)this.getGameObject().getComponent(EComponentType.AGraphic);
         if(g != null) {
-
              g.top = (g.top + 1) % 50;
              g.left = (g.left - 1) % 50;
         }
