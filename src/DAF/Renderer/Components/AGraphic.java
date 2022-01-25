8bfc313cf87ab9c78d5b7788f2e27a2083b39268
Friedrich Schmidt <friedrich.schmidt@stud.sbg.ac.at>
2022-01-25 13:35:25 +0100

    Added backgroundColor and borderRadius for AGraphics/ButtonGraphic. Improved sceneGraph handling for responsive UI buttons.

--- src/DAF/Renderer/Components/AGraphic.java
+++ src/DAF/Renderer/Components/AGraphic.java
@@ -10,6 +10,18 @@ public abstract class AGraphic extends AbstractComponent {
 
 	@JsonElement
 	Integer RenderingLayer = 1;
+
+	@JsonElement
+	boolean fillWidth = false;
+
+	@JsonElement
+	boolean fillHeight = false;
+
+	@JsonElement
+	String color = "rgba(0, 0, 0, 1.0)";
+
+	@JsonElement
+	String backgroundColor = "rgba(255, 255, 255, 1.0)";
 	
 	@Override
 	public void start() {
@@ -21,8 +33,24 @@ public abstract class AGraphic extends AbstractComponent {
 	}
 
 	public void setRenderingLayer(Integer level) {
-		this.RenderingLayer = level;		
+		this.RenderingLayer = level;
 	}
+
+	public void bindWidthToParent(boolean state) {
+		this.fillWidth = state;
+	}
+
+	public void bindHeightToParent(boolean state) {
+		this.fillHeight = state;
+	}
+
+	public String getWebColor() { return this.color; }
+
+	public void setWebColor(String color) { this.color = color; }
+
+	public String getWebBgColor() { return this.backgroundColor; }
+
+	public void setWebBgColor(String color) { this.backgroundColor = color; }
 
 	@Init
 	private void initValues() {
