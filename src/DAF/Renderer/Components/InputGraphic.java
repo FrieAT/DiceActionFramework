536b1c50c469af3c42585f80b6da98c71c79f88b
Friedrich Schmidt <friedrich.schmidt@stud.sbg.ac.at>
2022-01-26 13:19:39 +0100

    Moved width, height and positioning to AGraphic in order to be compliant with assumed json on rendering side.

--- src/DAF/Renderer/Components/InputGraphic.java
+++ src/DAF/Renderer/Components/InputGraphic.java
@@ -9,10 +9,6 @@ public class InputGraphic extends AGraphic {
     @JsonElement
     private Integer fontSize = 12;
     @JsonElement
-    private Integer width = 0;
-    @JsonElement
-    private Integer height = 0;
-    @JsonElement
     Boolean bold = false;
     @JsonElement
     String text ="Example Text";
@@ -21,27 +17,7 @@ public class InputGraphic extends AGraphic {
     public void start() {
         super.start();
     }
-
-    public Integer getWidth() {
-        return width;
-    }
-    public void setWidth(Integer width) {
-        this.width = width;
-    }
-    public Integer getHeight() {
-        return height;
-    }
-    public void setHeight(Integer height) {
-        this.height = height;
-    }
-    public Integer getTop() {
-        return this.getTransform().getGlobalPosition().y.intValue();
-    }
-    public void setTop(Integer top) { this.getTransform().setPosition(new Vector2(this.getLeft(), top)); }
-    public Integer getLeft() {
-        return this.getTransform().getGlobalPosition().x.intValue();
-    }
-    public void setLeft(Integer left) { this.getTransform().setPosition(new Vector2(left, this.getTop())); }
+    
     public Integer getFontSize() {
         return fontSize;
     }
--- src/DAF/Renderer/Components/PictureGraphic.java
+++ src/DAF/Renderer/Components/PictureGraphic.java
@@ -7,18 +7,13 @@ import DAF.Serializer.Serializable;
 
 @Serializable
 public class PictureGraphic extends AGraphic {
-
 	@JsonElement
-	private Integer width = 0;
-	@JsonElement
-	private Integer height = 0;
-	@JsonElement
-	String picturePath;
+	String picturePath = "";
 	
 	public PictureGraphic() {
 		super();
-		
-		backgroundColor = "rgba(255, 255, 255, 0.0)";
+
+		this.backgroundColor = "rgba(255, 255, 255, 0.0)";
 	}
 
 	@Override
@@ -26,40 +21,10 @@ public class PictureGraphic extends AGraphic {
 		super.start();
 	}
 
-	public Integer getWidth() {
-		return width;
-	}
-	public void setWidth(Integer width) {
-		this.width = width;
-	}
-	public Integer getHeight() {
-		return height;
-	}
-	public void setHeight(Integer height) {
-		this.height = height;
-	}
-	public Integer getTop() {
-		return (int)this.getTransform().getGlobalPosition().y.intValue();
-	}
-	public void setTop(Integer top) {
-		this.getTransform().setPosition(new Vector2(this.getLeft(), top));
-	}
-	public Integer getLeft() {
-		return this.getTransform().getGlobalPosition().x.intValue();
-	}
-	public void setLeft(Integer left) {
-		this.getTransform().setPosition(new Vector2(left.doubleValue(), this.getTop().doubleValue()));
-	}
 	public String getPicturePath() {
 		return picturePath;
 	}
 	public void setPicturePath(String picturePath) {
 		this.picturePath = picturePath;
-	}
-
-
-	@Init
-	private void initValues() {
-
 	}
 }
