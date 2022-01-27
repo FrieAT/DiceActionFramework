279f4db18afccaa8b0e58a4c0ec3319a44d048c6
Friedrich Schmidt <friedrich.schmidt@stud.sbg.ac.at>
2022-01-27 09:55:11 +0100

    Fix Faces is null.

--- src/DAF/Dice/Components/ADice.java
+++ src/DAF/Dice/Components/ADice.java
@@ -56,6 +56,9 @@ public abstract class ADice extends AbstractComponent implements IDice {
     }
 
     public Face getFace(int idx) {
+        if(idx >= diceFaces.size()) {
+            return null;
+        }
         return diceFaces.get(idx);
     }
 
